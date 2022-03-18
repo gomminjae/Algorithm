72b17cf91dd16f22e16e46e02c75ee8f59687df9
gomminjae <48856104+gomminjae@users.noreply.github.com>
2022-03-18 10:41:12 +0900

    Greedy

---
+++ Boj/로프(2217).py
@@ -0,0 +1,17 @@
+import sys
+from typing import Reversible 
+input = sys.stdin.readline 
+
+n = int(input())
+
+li = [] 
+result = []
+for _ in range(n):
+    li.append(int(input()))
+
+li.sort(reverse=True)
+
+for i in range(n):
+    result.append(li[i]*(i+1))
+
+print(max(result))
